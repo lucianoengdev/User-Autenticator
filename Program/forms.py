cb8aeda10b1c0d2de3fe228d01092a06cc6a0f07
Luciano Faria <luciano15faria@gmail.com>
2025-11-29 20:24:42 -0300

    1.0.0 Create a form

--- Program/forms.py
+++ Program/forms.py
@@ -1,1 +1,11 @@
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import Length, DataRequired
+
+
+class registro(FlaskForm):
+    name = StringField('Nome', validators=[DataRequired()])
+    user = StringField('Nome de Usuário', validators=[Length(max=15), DataRequired()])
+    password1 = StringField('Senha', validators=[Length(min=6, max=12), DataRequired()])
+    password2 = StringField('Confirme sua senha', validators=[Length(min=6, max=12), DataRequired()])
+
