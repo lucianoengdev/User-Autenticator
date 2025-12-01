220eb34f0551a582039c373e81fea34c6fbfce27
Luciano Faria <luciano15faria@gmail.com>
2025-12-01 18:08:53 -0300

    1.0.0 Function to create db and after I checkeded if it is correct

--- Program/init.py
+++ Program/init.py
@@ -1,8 +1,20 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+con = sqlite3.connect('users.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                name TEXT NOT NULL, 
+                                email TEXT UNIQUE NOT NULL,
+                                password TEXT NOT NULL)''')
+cur.close()
+con.close()
+
+
 app.config['SECRET_KEY'] = '123456'
+
 
 from Program import forms
 from Program import routes
