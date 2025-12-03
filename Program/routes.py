921951ff2d1fe3730541ee1732bda913d9e88e25
Luciano Faria <luciano15faria@gmail.com>
2025-12-02 21:21:35 -0300

    1.0.0 Updated routes function to insert register in db

--- Program/routes.py
+++ Program/routes.py
@@ -1,6 +1,7 @@
-from .init import app
-from flask import render_template, url_for, redirect
+from .init import app, db
+from flask import render_template, url_for, redirect, flash
 from .forms import registro
+from .models import User
 
 @app.route('/')
 @app.route('/home')
@@ -12,6 +13,21 @@ def home():
 def register():
     form = registro()
     if form.validate_on_submit():
+
+        if db.session.query(User).filter_by(email=form.email.data).first():
+            flash("Esse email já existe!")
+            return render_template("register.html", form=form)
+        
+        user_to_create = User(name = form.name.data,
+                              email = form.email.data,
+                              password = form.password1.data
+        )
+
+        db.session.add(user_to_create)
+        db.session.commit()
+        db.create_all()
+        
+         
         return redirect(url_for('home'))
     return render_template("register.html", form=form)
 
