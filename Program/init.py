6361a18856fe1e6301f993c75284cb1b6465a245
Luciano Faria <luciano15faria@gmail.com>
2025-12-05 17:52:49 -0300

    1.0.0 Configuring password hash in init.py

--- Program/init.py
+++ Program/init.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.login_view = "login"
 login_manager.init_app(app)
