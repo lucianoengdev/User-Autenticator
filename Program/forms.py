f01e54c24164381d3c2a946dab14aa2534ccbb8a
Luciano Faria <luciano15faria@gmail.com>
2025-12-05 17:52:18 -0300

    1.0.0 Configuring password hash

--- Program/forms.py
+++ Program/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import Length, DataRequired
+from wtforms.validators import Length, DataRequired, EqualTo
 
 
 class registro(FlaskForm):
     name = StringField('Nome', validators=[DataRequired()])
     email = StringField('Email', validators=[Length(max=30), DataRequired()])
     password1 = StringField('Senha', validators=[Length(min=6, max=12), DataRequired()])
-    password2 = StringField('Confirme sua senha', validators=[Length(min=6, max=12), DataRequired()])
+    password2 = StringField('Confirme sua senha', validators=[Equalto('password1'), DataRequired()])
 
