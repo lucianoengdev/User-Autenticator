58a14273b4dfc10426657f5eb352f15991affe2f
Luciano Faria <luciano15faria@gmail.com>
2025-12-06 14:38:47 -0300

    1.0.0 Was using EqualTo wrongly

--- Program/forms.py
+++ Program/forms.py
@@ -7,5 +7,5 @@ class registro(FlaskForm):
     name = StringField('Nome', validators=[DataRequired()])
     email = StringField('Email', validators=[Length(max=30), DataRequired()])
     password1 = StringField('Senha', validators=[Length(min=6, max=12), DataRequired()])
-    password2 = StringField('Confirme sua senha', validators=[Equalto('password1'), DataRequired()])
+    password2 = StringField('Confirme sua senha', validators=[EqualTo('password1'), DataRequired()])
 
