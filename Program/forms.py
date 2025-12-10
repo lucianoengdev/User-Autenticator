0b57f84e26690465b751b358f8b252941dbd1be6
Luciano Faria <luciano15faria@gmail.com>
2025-12-10 20:02:46 -0300

    1.2.0 Create a new form for a new function of the program

--- Program/forms.py
+++ Program/forms.py
@@ -10,17 +10,17 @@ class registro(FlaskForm):
     password2 = StringField('Confirme sua senha', validators=[EqualTo('password1'), DataRequired()])
 
 class CalculoFerroviario(FlaskForm):
-    velocidade = FloatField('Velocidade Diretriz (V) [km/h]', validators=[DataRequired()])
-    raio = FloatField('Raio da Curva (R) [m]', validators=[DataRequired()])
-    bitola = FloatField('Bitola (b) [m]', validators=[DataRequired()])
-    largura_boleto = FloatField('Largura do Boleto (Lb) [m]', validators=[DataRequired()])
+    velocidade = FloatField('Velocidade Diretriz (V) [km/h]', validators=[InputRequired()])
+    raio = FloatField('Raio da Curva (R) [m]', validators=[InputRequired()])
+    bitola = FloatField('Bitola (b) [m]', validators=[InputRequired()])
+    largura_boleto = FloatField('Largura do Boleto (Lb) [m]', validators=[InputRequired()])
     
-    altura_cg = FloatField('Altura do CG (H) [m]', validators=[DataRequired()])
-    coef_seguranca = FloatField('Coeficiente de Segurança (η)', validators=[DataRequired()], default=5.0)
-    deslocamento_cg = FloatField('Deslocamento do CG (d) [m]', validators=[DataRequired()], default=0.1)
+    altura_cg = FloatField('Altura do CG (H) [m]', validators=[InputRequired()])
+    coef_seguranca = FloatField('Coeficiente de Segurança (η)', validators=[InputRequired()], default=5.0)
+    deslocamento_cg = FloatField('Deslocamento do CG (d) [m]', validators=[InputRequired()], default=0.1)
     
-    aceleracao_jc = FloatField('Aceleração não compensada (Jc) [m/s²]', validators=[DataRequired()], default=0.65)
-    gravidade = FloatField('Gravidade (g) [m/s²]', validators=[DataRequired()], default=9.81)
+    aceleracao_jc = FloatField('Aceleração não compensada (Jc) [m/s²]', validators=[InputRequired()], default=0.65)
+    gravidade = FloatField('Gravidade (g) [m/s²]', validators=[InputRequired()], default=9.81)
     
     submit = SubmitField('Calcular Superelevação')
 
@@ -39,3 +39,20 @@ class CalculoVelocidade(FlaskForm):
     gravidade = FloatField('Gravidade (g) [m/s²]', validators=[InputRequired()], default=9.81)
     
     submit = SubmitField('Calcular Velocidades e Geometria')
+
+class CalculoTrilho(FlaskForm):
+    carga_por_eixo = FloatField('Carga por Eixo (Pe) [kgf]', validators=[InputRequired()])
+    velocidade = FloatField('Velocidade (V) [km/h]', validators=[InputRequired()])
+    
+    espacamento_dormente = FloatField('Espaçamento entre eixos (a) [cm]', validators=[InputRequired()])
+    largura_dormente = FloatField('Largura do Dormente (b) [cm]', validators=[InputRequired()])
+    coef_lastro = FloatField('Coef. de Lastro (C) [kgf/cm³]', validators=[InputRequired()])
+    
+    modulo_elasticidade = FloatField('Módulo de Elasticidade (E) [kgf/cm²]', validators=[InputRequired()], default=2100000)
+    momento_inercia = FloatField('Momento de Inércia (I) [cm⁴]', validators=[InputRequired()])
+    modulo_resistencia = FloatField('Módulo de Resistência (W) [cm³]', validators=[InputRequired()])
+    
+    tensao_admissivel = FloatField('Tensão Admissível (σ adm) [kgf/cm²]', 
+                                   validators=[InputRequired(), NumberRange(min=1500, max=1750, message="A tensão deve estar entre 1500 e 1750 conforme norma AREMA")])
+    
+    submit = SubmitField('Verificar Trilho')
